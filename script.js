a268b7bbbb29bc386bd1d39057bc49943743e947
greg@gregPredator <greg19850@gmail.com>
2022-07-08 21:01:58 +0100

    created function showing mouse position and changing background color

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const h1 = document.querySelector('h1');
 
 
-const changeBackgroundColor = () => {
+const changeBackgroundColor = (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
 
+    h1.textContent = `${x}, ${y}`
+
+    let red = x / window.innerWidth * 100
+    let green = y / window.innerHeight * 100
+    let blue = ((x / window.innerWidth + y / window.innerHeight) * 100) / 2
+
+    document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
 }
+
 
 document.body.addEventListener('mousemove', changeBackgroundColor)
